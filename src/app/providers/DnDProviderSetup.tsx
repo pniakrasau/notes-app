1694ee5cc0f6002dc11274324f9f9221c30ea4e3
pniakrasau <pavel.nekrasof.upwork@gmail.com>
2024-08-15 13:01:54 +0200

    fix(notes): fix mobile drag and drop

--- src/app/providers/DnDProviderSetup.tsx
+++ src/app/providers/DnDProviderSetup.tsx
@@ -1,11 +1,16 @@
 import type { ReactElement, ReactNode } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 
 type Props = {
   children: ReactNode;
 };
 
 export function DnDProviderSetup({ children }: Props): ReactElement {
-  return <DndProvider backend={HTML5Backend}>{children}</DndProvider>;
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={TouchBackend}>{children}</DndProvider>
+    </DndProvider>
+  );
 }
